66e2cf6d68a8ecfa05ad491cbbdfee466d7ba4b9
Izyan Izzati <37804516+i96yan@users.noreply.github.com>
2018-05-22 00:52:14 +0800

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -4,7 +4,7 @@
 <center><img id="banner" src="http://www.calicuttaxi.com/images/banner_04.png" alt="Banner Image" width="1000" height="250"></center>
 <body background="http://media.istockphoto.com/photos/abstract-blue-green-technology-and-science-modern-background-picture-id523760376?k=6&m=523760376&s=170667a&w=0&h=aftceF6b8PRPLj5ATpYHvA7a9g-KgxLBOihz9sT_3PM=">
 //this page describes about the organization
-<title>About Us</title>
+<title>About Us and FAq</title>
 <style type="text/css">
 
 body
